d83ab2ab9510aa7feff4bd91ab4fa2554efa9a51
Eris-F <erisfrangu@gmail.com>
2021-06-12 10:49:21 +0100

    Project as of 12June 2021

---
+++ scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
+
+// Scroll into view 
+
+function homefunction() {
+    var element = document.getElementById("Home");
+    element.scrollIntoView();
+  }
+
+function amfunction() {
+    var element = document.getElementById("AboutMe");
+    element.scrollIntoView();
+  }
+
+function prfunction() {
+    var element = document.getElementById("Projects");
+    element.scrollIntoView();
+  }
